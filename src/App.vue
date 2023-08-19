c894bd4c85dcfe7d144bc8be30ea8e5e46e1a75b
Armfath <info.armanangou@gmail.com>
2023-08-20 00:35:47 +0100

    feat: add header and footer

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <img src="./assets/icon_weather_app.svg" alt="">
+        <span class="ml-3 text-xl">Weather App</span>
+      </a>
+    </div>
+  </header>
+  <section>
+
+  </section>
+
+
+  <footer class="text-gray-600 body-font">
+    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+     
+      <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Copyright —
+        <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">Weather App</a>
+      </p>
+     
+    </div>
+  </footer>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
