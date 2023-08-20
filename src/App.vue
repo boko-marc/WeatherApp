c8cdaef64d9fbc0737293e46c21b810de608a9cb
Armfath <info.armanangou@gmail.com>
2023-08-20 20:26:31 +0100

    feat: add WeatherCard,CitySearch components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,54 @@
 <template>
-<div>
-  <header class="text-gray-600 body-font">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <img src="./assets/icon_weather_app.svg" alt="">
-        <span class="ml-3 text-xl">Weather App</span>
-      </a>
-    </div>
-  </header>
-  <section>
+  <div class="container mx-auto max-w-md bg-red-300 mt-4 md:mt-32 rounded-2xl px-4">
+    <header class="text-gray-600 body-font">
+      <div
+        class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
+      >
+        <a
+          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
+          <img src="./assets/icon_weather_app.svg" alt="" />
+          <span class="ml-3 text-xl">Weather App</span>
+        </a>
+      </div>
+    </header>
+    <section>
+      <CitySearch></CitySearch>
+      <WeatherCard></WeatherCard>
+    </section>
 
-  </section>
-
-
-  <footer class="text-gray-600 body-font">
-    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-     
-      <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Copyright —
-        <a href="https://twitter.com/knyttneve" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">Weather App</a>
-      </p>
-     
-    </div>
-  </footer>
-</div>
+    <footer class="text-gray-600 body-font">
+      <div
+        class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col"
+      >
+        <p
+          class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
+        >
+          © 2023 Copyright —
+          <a
+            href=""
+            class="text-gray-600 ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+            >Weather App</a
+          >
+        </p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
+import  CitySearch  from "./components/CitySearch.vue";
+import WeatherCard from './components/WeatherCard.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    CitySearch,WeatherCard
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/CitySearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <section class="text-gray-600 body-font">
+      <div
+        class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+      >
+        <div
+          class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+        >
+          <div class="flex w-full md:justify-start justify-center items-center">
+            <div class="relative mr-4">
+              <input
+                type="text"
+                id="hero-field"
+                name="hero-field"
+                placeholder="Veuillez saisir la ville"
+                class="bg-gray-100 rounded border bg-opacity-50 border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'CitySearch',
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
